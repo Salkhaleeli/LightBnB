9fdf69ac7e834fce84757fe953b16a3cbe8b1a69
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-07-27 15:55:49 -0600

    adding migrations and seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (
+  name, email, password)
+  VALUES (
+    'Saad Alkhaleeli', 'alkhaleelisaad@gmail.com',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
+  );
+  INSERT INTO users (
+  name, email, password)
+  VALUES('Mo Salam', 'mosalam@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+');
+INSERT INTO users (
+  name, email, password)
+  VALUES('atheer almayahi', 'almayahiatheer@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+');
